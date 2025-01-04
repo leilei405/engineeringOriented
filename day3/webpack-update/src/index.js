2ae1d479f54995ed6a7fe33d0de08aa1b83bb0eb
leilei405 <1601178425@qq.com>
2025-01-04 13:13:41 +0800

    feat: 迁移首页之在 src/index.js 中添加 css 引用

--- day3/webpack-update/src/index.js
+++ day3/webpack-update/src/index.js
@@ -1,1 +1,2 @@
-console.log('webpack')
+import './css/public.css';
+import './css/index.css';
--- day3/webpack-update/webpack.config.js
+++ day3/webpack-update/webpack.config.js
@@ -1,14 +1,46 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   devtool: 'source-map',
-  entry: './src/index.js',
+  // entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js'
+  },
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js'
+    // name 默认是main.js  hash 是每次编译生成一个新的文件
+    filename: 'js/[name].js'
   },
   module: {
-    rules: []
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: 'asset',
+        parser: {
+          // 小于4kb的图片转base64位
+          // 优点：减少请求数量  缺点：图片体积会更大
+          dataUrlCondition: {
+            maxSize: 4 * 1024
+          }
+        },
+        generator: {
+          // 输出图片名称
+          // [hash:8] hash值取8位  [ext] 文件扩展名
+          filename: 'images/[name].[hash:4][ext]'
+        }
+      }
+    ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html', // 模版名称
+      template: path.resolve(__dirname, './src/index.html') // 模版路径
+    })
+  ]
 }
