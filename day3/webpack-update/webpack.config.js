c77a111b33755083e31071b1fda5f9e0b935204f
leilei405 <1601178425@qq.com>
2025-01-04 15:03:35 +0800

    feat: 开发模式配置

--- day3/webpack-update/webpack.config.js
+++ day3/webpack-update/webpack.config.js
@@ -1,20 +1,29 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+  // 开发模式
   mode: 'development',
+
+  // 开启source-map
   devtool: 'source-map',
-  // entry: './src/index.js',
-  entry: {
+
+  // 入口文件
+  // entry: './src/index.js', // 单入口
+  entry: {                    // 多入口
     index: './src/index.js',
     login: './src/login.js'
   },
+
+  // 输出
   output: {
     path: path.resolve(__dirname, './dist'),
-    // name 默认是main.js  hash 是每次编译生成一个新的文件
-    filename: 'js/[name].js'
+    filename: 'js/[name].js',  // name 默认是main.js  hash 是每次编译生成一个新的文件
   },
+
+  // 模块
   module: {
     rules: [
       {
@@ -39,13 +48,16 @@ module.exports = {
       }
     ]
   },
+
   plugins: [
+    // 模版配置1
     new HtmlWebpackPlugin({
         filename: 'index.html', // 模版名称
         template: path.resolve(__dirname, './src/index.html'),   // 模版路径
         chunks: ['index'] // 引入的js文件
     }),
 
+    // 模版配置2
     new HtmlWebpackPlugin({
       filename: 'login.html', // 模版名称
       template: path.resolve(__dirname, './src/login.html'),   // 模版路径
@@ -56,6 +68,25 @@ module.exports = {
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery'
+    }),
+
+    // 拷贝文件
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, './src/img'),
+          to: path.resolve(__dirname, './dist/img')
+        }
+      ]
     })
-  ]
+  ],
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'), // 构建后的结果放在内存中
+    },
+    compress: true, // 是否启动压缩
+    port: 8888, // 端口
+    hot: true, // 是否热更新
+  }
 }
