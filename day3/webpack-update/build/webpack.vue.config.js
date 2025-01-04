d662ff4fb3439744f2ca0eb8e0b98515f72914c7
leilei405 <1601178425@qq.com>
2025-01-05 01:21:43 +0800

    feat: 编写Vue项目构建配置文件

---
+++ day3/webpack-update/build/webpack.vue.config.js
@@ -0,0 +1,91 @@
+const path = require('path');
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const CssMinimizerWebpackPlugin = require("css-minimizer-webpack-plugin");
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+const config = {
+  mode: 'development',
+
+  entry: {
+    index: path.resolve(__dirname, '../src/main.js')
+  },
+
+  output: {
+    filename: 'js/[name].js',
+    path: path.resolve(__dirname, '../dist')
+  },
+
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, '../dist'), // 构建后的结果放在内存中
+    },
+    port: 8000,
+    open: true,
+    hot: true
+  },
+
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserWebpackPlugin(),
+      new CssMinimizerWebpackPlugin()
+    ],
+    splitChunks: {
+      minSize: 500 * 1024,
+      chunks: 'all',
+      name: 'common'
+    }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 8 * 1024
+          }
+        },
+        generator: {
+          filename: 'images/[name].[hash:4][ext]'
+        }
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, '../public/index.html'),
+      chunks: ['index']
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{
+        from: path.resolve(__dirname, '../src/img'),
+        to: path.resolve(__dirname, '../dist/img')
+      }]
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+      chunkFilename: 'css/[name].chunk.css'
+    }),
+    new VueLoaderPlugin()
+  ],
+}
+
+module.exports = config
---
+++ day3/webpack-update/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'Hello Vue!'
+    }
+  }
+}
+
+</script>
+
+<template>
+<div>{{ message }}</div>
+</template>
+
+<style scoped>
+
+</style>
