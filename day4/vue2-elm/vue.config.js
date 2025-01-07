6b84d5bc7ea17cdaa2378b97278cc35c57c6c729
leilei405 <1601178425@qq.com>
2025-01-08 01:07:19 +0800

    feat: mage-webpack-loader实现图片5倍压缩

--- day4/vue2-elm/vue.config.js
+++ day4/vue2-elm/vue.config.js
@@ -29,6 +29,33 @@ module.exports = {
     module: {
       rules: [
         {
+          test: /\.(png|gif|jpe?g|sv g|webp)$/i,
+          use: [
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                mozjpeg: {
+                  progressive: true,
+                  quality: 65
+                },
+                optipng: {
+                  enabled: false,
+                },
+                pngquant: {
+                  quality: [0.65, 0.90],
+                  speed: 4
+                },
+                gifsicle: {
+                  interlaced: false,
+                },
+                webp: {
+                  quality: 75
+                }
+              }
+            }
+          ]
+        },
+        {
           test: /\.js$/,
           exclude: /node_modules/,
           include: path.resolve(__dirname, './src'),
