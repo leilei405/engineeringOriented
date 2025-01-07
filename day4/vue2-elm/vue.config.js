5675f9746aa01904219f6c3a59b74429897813f4
leilei405 <1601178425@qq.com>
2025-01-07 22:09:55 +0800

    feat: 深入分析构建体积检测插件webpack-bundle-analyzer

--- day4/vue2-elm/vue.config.js
+++ day4/vue2-elm/vue.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
+
 
 const smp = new SpeedMeasurePlugin({
   disable: !(process.env.MEASURE === 'true'), // 是否禁用插件
@@ -14,6 +16,14 @@ module.exports = {
         'assets': path.resolve(__dirname, './src/assets'),
         'components': path.resolve(__dirname, './src/components'),
       }
-    }
+    },
+    plugins: [
+      new BundleAnalyzerPlugin({
+        // analyzerMode: 'disabled', // 不启动展示打包报告的http服务器
+        // analyzerMode: 'server', // 默认展示打开打包报告的http服务器
+        // analyzerMode: 'static', // 生成打包报告文件 html
+        // generateStatsFile: true, // 是否生成stats.json  文件
+      }),
+    ],
   })
 }
