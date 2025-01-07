2a4f0eb214c180ab652d58989ac2c4e610fb860d
leilei405 <1601178425@qq.com>
2025-01-07 20:02:57 +0800

    feat: 对vue2 项目进行依赖升级 项目打包配置重构 以及用的语法问题解决 各种报错解决

--- day4/vue2-elm/src/router/router.js
+++ day4/vue2-elm/src/router/router.js
@@ -1,55 +1,52 @@
+import { createRouter, createWebHistory } from 'vue-router'
 import App from '../App'
+import Home from '../page/home/home'
+import City from '../page/city/city'
+import Msite from '../page/msite/msite'
+import Search from '../page/search/search'
+import Shop from '../page/shop/shop'
+import Login from '../page/login/login'
+import Profile from '../page/profile/profile'
+import Forget from '../page/forget/forget'
+import Order from '../page/order/order'
+import OrderDetail from '../page/order/children/orderDetail'
+import Vipcard from '../page/vipcard/vipcard'
+import InvoiceRecord from '../page/vipcard/children/invoiceRecord'
+import UseCart from '../page/vipcard/children/useCart'
+import VipDescription from '../page/vipcard/children/vipDescription'
+import Food from '../page/food/food'
+import ConfirmOrder from '../page/confirmOrder/confirmOrder'
+import Remark from '../page/confirmOrder/children/remark'
+import Payment from '../page/confirmOrder/children/payment'
+import UserValidation from '../page/confirmOrder/children/userValidation'
+import Invoice from '../page/confirmOrder/children/invoice'
+import ChooseAddress from '../page/confirmOrder/children/chooseAddress'
+import AddAddress from '../page/confirmOrder/children/children/addAddress'
+import SearchAddress from '../page/confirmOrder/children/children/children/searchAddress'
+import FoodDetail from '../page/shop/children/foodDetail'
+import ShopDetail from '../page/shop/children/shopDetail'
+import ShopSafe from '../page/shop/children/children/shopSafe'
+import Info from '../page/profile/children/info'
+import Setusername from '../page/profile/children/children/setusername'
+import Address from '../page/profile/children/children/address'
+import Add from '../page/profile/children/children/children/add'
+import AddDetail from '../page/profile/children/children/children/children/addDetail'
+import Balance from '../page/balance/balance'
+import BalanceDetail from '../page/balance/children/detail'
+import Benefit from '../page/benefit/benefit'
+import Coupon from '../page/benefit/children/coupon'
+import HbDescription from '../page/benefit/children/hbDescription'
+import HbHistory from '../page/benefit/children/hbHistory'
+import Exchange from '../page/benefit/children/exchange'
+import Commend from '../page/benefit/children/commend'
+import Points from '../page/points/points'
+import PointsDetail from '../page/points/children/detail'
+import Service from '../page/service/service'
+import QuestionDetail from '../page/service/children/questionDetail'
+import Find from '../page/find/find'
+import Download from '../page/download/download'
 
-const home = r => require.ensure([], () => r(require('../page/home/home')), 'home')
-const city = r => require.ensure([], () => r(require('../page/city/city')), 'city')
-const msite = r => require.ensure([], () => r(require('../page/msite/msite')), 'msite')
-const search = r => require.ensure([], () => r(require('../page/search/search')), 'search')
-const shop = r => require.ensure([], () => r(require('../page/shop/shop')), 'shop')
-const login = r => require.ensure([], () => r(require('../page/login/login')), 'login')
-const profile = r => require.ensure([], () => r(require('../page/profile/profile')), 'profile')
-const forget = r => require.ensure([], () => r(require('../page/forget/forget')), 'forget')
-const order = r => require.ensure([], () => r(require('../page/order/order')), 'order')
-const orderDetail = r => require.ensure([], () => r(require('../page/order/children/orderDetail')), 'orderDetail')
-const vipcard = r => require.ensure([], () => r(require('../page/vipcard/vipcard')), 'vipcard')
-const invoiceRecord = r => require.ensure([], () => r(require('../page/vipcard/children/invoiceRecord')), 'invoiceRecord')
-const useCart = r => require.ensure([], () => r(require('../page/vipcard/children/useCart')), 'useCart')
-const vipDescription = r => require.ensure([], () => r(require('../page/vipcard/children/vipDescription')), 'vipDescription')
-const food = r => require.ensure([], () => r(require('../page/food/food')), 'food')
-const confirmOrder = r => require.ensure([], () => r(require('../page/confirmOrder/confirmOrder')), 'confirmOrder')
-const remark = r => require.ensure([], () => r(require('../page/confirmOrder/children/remark')), 'remark')
-const payment = r => require.ensure([], () => r(require('../page/confirmOrder/children/payment')), 'payment')
-const userValidation = r => require.ensure([], () => r(require('../page/confirmOrder/children/userValidation')), 'userValidation')
-const invoice = r => require.ensure([], () => r(require('../page/confirmOrder/children/invoice')), 'invoice')
-const chooseAddress = r => require.ensure([], () => r(require('../page/confirmOrder/children/chooseAddress')), 'chooseAddress')
-const addAddress = r => require.ensure([], () => r(require('../page/confirmOrder/children/children/addAddress')), 'addAddress')
-const searchAddress = r => require.ensure([], () => r(require('../page/confirmOrder/children/children/children/searchAddress')), 'searchAddress')
-const foodDetail = r => require.ensure([], () => r(require('../page/shop/children/foodDetail')), 'foodDetail')
-const shopDetail = r => require.ensure([], () => r(require('../page/shop/children/shopDetail')), 'shopDetail')
-const shopSafe = r => require.ensure([], () => r(require('../page/shop/children/children/shopSafe')), 'shopSafe')
-const info = r => require.ensure([], () => r(require('../page/profile/children/info')), 'info')
-const setusername = r => require.ensure([], () => r(require('../page/profile/children/children/setusername')), 'setusername')
-const address = r => require.ensure([], () => r(require('../page/profile/children/children/address')), 'address')
-const add = r => require.ensure([], () => r(require('../page/profile/children/children/children/add')), 'add')
-const addDetail = r => require.ensure([], () => r(require('../page/profile/children/children/children/children/addDetail')), 'addDetail')
-const balance = r => require.ensure([], () => r(require('../page/balance/balance')), 'balance')
-const balanceDetail = r => require.ensure([], () => r(require('../page/balance/children/detail')), 'balanceDetail')
-const benefit = r => require.ensure([], () => r(require('../page/benefit/benefit')), 'benefit')
-const coupon = r => require.ensure([], () => r(require('../page/benefit/children/coupon')), 'coupon')
-const hbDescription = r => require.ensure([], () => r(require('../page/benefit/children/hbDescription')), 'hbDescription')
-const hbHistory = r => require.ensure([], () => r(require('../page/benefit/children/hbHistory')), 'hbHistory')
-const exchange = r => require.ensure([], () => r(require('../page/benefit/children/exchange')), 'exchange')
-const commend = r => require.ensure([], () => r(require('../page/benefit/children/commend')), 'commend')
-const points = r => require.ensure([], () => r(require('../page/points/points')), 'points')
-const pointsDetail = r => require.ensure([], () => r(require('../page/points/children/detail')), 'pointsDetail')
-const service = r => require.ensure([], () => r(require('../page/service/service')), 'service')
-const questionDetail = r => require.ensure([], () => r(require('../page/service/children/questionDetail')), 'questionDetail')
-const find = r => require.ensure([], () => r(require('../page/find/find')), 'find')
-const download = r => require.ensure([], () => r(require('../page/download/download')), 'download')
-
-
-
-
-export default [{
+const routes =  [{
     path: '/',
     component: App, //顶层路由，对应index.html
     children: [ //二级路由。对应App.vue
@@ -61,70 +58,70 @@ export default [{
         //首页城市列表页
         {
             path: '/home',
-            component: home
+            component: Home
         },
         //当前选择城市页
         {
             path: '/city/:cityid',
-            component: city
+            component: City
         },
         //所有商铺列表页
         {
             path: '/msite',
-            component: msite,
+            component: Msite,
             meta: { keepAlive: true },
         },
         //特色商铺列表页
         {
             path: '/food',
-            component: food
+            component: Food
         },
         //搜索页
         {
             path: '/search/:geohash',
-            component: search
+            component: Search
         },
         //商铺详情页
         {
             path: '/shop',
-            component: shop,
+            component: Shop,
             children: [{
                 path: 'foodDetail', //食品详情页
-                component: foodDetail,
+                component: FoodDetail,
             }, {
                 path: 'shopDetail', //商铺详情页
-                component: shopDetail,
+                component: ShopDetail,
                 children: [{
                     path: 'shopSafe', //商铺安全认证页
-                    component: shopSafe,
+                    component: ShopSafe,
                 }, ]
             }]
         },
         //确认订单页
         {
             path: '/confirmOrder',
-            component: confirmOrder,
+            component: ConfirmOrder,
             children: [{
                 path: 'remark', //订单备注
-                component: remark,
+                component: Remark,
             }, {
                 path: 'invoice', //发票抬头
-                component: invoice,
+                component: Invoice,
             }, {
                 path: 'payment', //付款页面
-                component: payment,
+                component: Payment,
             }, {
                 path: 'userValidation', //用户验证
-                component: userValidation,
+                component: UserValidation,
             }, {
                 path: 'chooseAddress', //选择地址
-                component: chooseAddress,
+                component: ChooseAddress,
                 children: [{
                     path: 'addAddress', //添加地址
-                    component: addAddress,
+                    component: AddAddress,
                     children: [{
                         path: 'searchAddress', //搜索地址
-                        component: searchAddress,
+                        component: SearchAddress,
                     }]
                 }, ]
             }, ]
@@ -132,122 +129,129 @@ export default [{
         //登录注册页
         {
             path: '/login',
-            component: login
+            component: Login
         },
         //个人信息页
         {
             path: '/profile',
-            component: profile,
+            component: Profile,
             children: [{
                 path: 'info', //个人信息详情页
-                component: info,
+                component: Info,
                 children: [{
                     path: 'setusername',
-                    component: setusername,
+                    component: Setusername,
                 },{
                     path: 'address',
-                    component: address,     //编辑地址
+                    component: Address,     //编辑地址
                     children:[{
                         path:'add',
-                        component:add,
+                        component:Add,
                         children:[{
                             path:'addDetail',
-                            component:addDetail
+                            component: AddDetail
                         }]
                     }]
                 }]
             },
             {
                 path: 'service', //服务中心
-                component: service,
+                component: Service,
             },]
         },
         //修改密码页
         {
             path: '/forget',
-            component: forget
+            component: Forget
         },
         //订单列表页
         {
             path: '/order',
-            component: order,
+            component: Order,
             children: [{
                 path: 'orderDetail', //订单详情页
-                component: orderDetail,
+                component: OrderDetail,
             }, ]
         },
         //vip卡页
         {
             path: '/vipcard',
-            component: vipcard,
+            component: Vipcard,
             children: [{
                 path: 'invoiceRecord', //开发票
-                component: invoiceRecord,
+                component: InvoiceRecord,
             }, {
                 path: 'useCart', //购买会员卡
-                component: useCart,
+                component: UseCart,
             }, {
                 path: 'vipDescription', //会员说明
-                component: vipDescription,
+                component: VipDescription,
             },]
         },
         //发现页
         {
             path: '/find',
-            component: find
+            component: Find
         },
         //下载页
         {
             path: '/download',
-            component: download
+            component: Download
         },
         //服务中心
         {
             path: '/service',
-            component: service,
+            component: Service,
              children: [{
                 path: 'questionDetail', //订单详情页
-                component: questionDetail,
+                component: QuestionDetail,
             }, ]
         },
         //余额
         {
             path: 'balance',
-            component: balance,
+            component: Balance,
             children: [{
                 path: 'detail', //余额说明
-                component: balanceDetail,
+                component: BalanceDetail,
             }, ]
         },
         //我的优惠页
         {
             path: 'benefit',
-            component: benefit,
+            component: Benefit,
             children: [{
                 path: 'coupon', //代金券说明
-                component: coupon,
+                component: Coupon,
             }, {
                 path: 'hbDescription', //红包说明
-                component: hbDescription,
+                component: HbDescription,
             }, {
                 path: 'hbHistory', //历史红包
-                component: hbHistory,
+                component: HbHistory,
             }, {
                 path: 'exchange', //兑换红包
-                component: exchange,
+                component: Exchange,
             }, {
                 path: 'commend', //推荐有奖
-                component: commend,
+                component: Commend,
             },]
         },
         //我的积分页
         {
             path: 'points',
-            component: points,
+            component: Points,
             children: [{
                 path: 'detail', //积分说明
-                component: pointsDetail,
+                component: PointsDetail,
             }, ]
         },
     ]
-}]
+}];
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+export default router;
