974b8df473124e0a80b127d2ee3ddf2f092fd6ba
leilei405 <1601178425@qq.com>
2025-01-07 21:40:08 +0800

    feat: 深入分析构建速度测量插件speed-measure-webpack-plugin

--- day4/vue2-elm/vue.config.js
+++ day4/vue2-elm/vue.config.js
@@ -1,7 +1,13 @@
-const path = require('path')
+const path = require('path');
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
+
+const smp = new SpeedMeasurePlugin({
+  disable: !(process.env.MEASURE === 'true'), // 是否禁用插件
+  outputFormat: 'humanVerbose', // 输出格式 比较详细的输出
+});
 
 module.exports = {
-  configureWebpack: {
+  configureWebpack: smp.wrap({
     resolve: {
       alias: {
         'src': path.resolve(__dirname, './src'),
@@ -9,5 +15,5 @@ module.exports = {
         'components': path.resolve(__dirname, './src/components'),
       }
     }
-  }
+  })
 }
