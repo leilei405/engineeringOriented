3ebc51fb57b62cc0902adb3a543e1f9edd4a0133
leilei405 <1601178425@qq.com>
2025-01-09 00:50:05 +0800

    feat: webpack5高级特性：TreeShaking和SideEffects

--- day5/webpack-new-feature/webpack4-demo/src/index.js
+++ day5/webpack-new-feature/webpack4-demo/src/index.js
@@ -1,7 +1,8 @@
-import data from 'data:text/javascript, export default "hello world"';
-
-console.log(data, '====')
+// import data from 'data:text/javascript, export default "hello world"';
+// console.log(data, '====')
+import './lib'
 console.log('hello webpack4');
+console.log('1111');
 
 const icon = require('./assets/img.png');
 const img = document.createElement('img');
--- day5/webpack-new-feature/webpack5-demo/src/index.js
+++ day5/webpack-new-feature/webpack5-demo/src/index.js
@@ -10,3 +10,6 @@ const icon = require('./assets/img.png');
 const img = document.createElement('img');
 img.src = icon;
 document.body.insertBefore(img, document.body.firstChild);
+import './lib.js'
+import 'vue'
+console.log('1111')
