7d5d91a6e35ccc2f283fef31e223efdb86e5e322
leilei405 <1601178425@qq.com>
2025-01-11 21:27:35 +0800

    feat: vite支持vue3项目开发

---
+++ day6/vite-test/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import test from './index.js'
+console.log(test.a)
+
+</script>
+
+<template>
+  <div>
+    <h1>Hello Vue3</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- day6/vite-test/src/index.js
+++ day6/vite-test/src/index.js
@@ -1,3 +1,6 @@
 import utils from './utils.js'
 console.log('hello vite')
 utils();
+export default {
+  a: 1
+}
