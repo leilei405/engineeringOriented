114c5d4235c5b2f26f082737a7c881d0b6a8ffaf
leilei405 <1601178425@qq.com>
2025-01-12 12:13:42 +0800

    feat: vite打包代码支持低版本浏览器运行之legacy @vitejs/plugin-legacy

--- day6/vite-test/vite.config.js
+++ day6/vite-test/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
   base: '/test/',
-  plugins: [vue()]
+  plugins: [vue(), legacy()]
 })
