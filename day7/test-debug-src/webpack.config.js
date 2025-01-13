78d9c3de5af3ba8f310bff02765146566dcbbd77
leilei405 <1601178425@qq.com>
2025-01-13 21:32:29 +0800

    feat: webpack 代码调试

---
+++ day7/test-debug-src/webpack.config.js
@@ -0,0 +1,11 @@
+const path =  require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'js/[name].js',
+  },
+}
