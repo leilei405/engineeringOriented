2ec4569d8fc9f6b2da1ce6f7bf17fcc5ecd80d14
leilei405 <1601178425@qq.com>
2025-02-20 00:20:28 +0800

    feat: 封装 get 方法，并练习下reduce

---
+++ day9/lerna-react19/src/study/algorithm/saftGet.js
@@ -0,0 +1,33 @@
+const obj = {
+  user: {
+    profile: {
+      name: 'Sunday',
+      age: 30
+    }
+  }
+};
+
+function safeGet (obj, path, defaultValue) {
+  const keys = path.split('.');
+
+  return keys.reduce((acc, key) => {
+    console.log(key, '====')
+    if (acc && acc.hasOwnProperty(key)) {
+      return acc[key]; // 返回存在的属性值
+    } else {
+      return defaultValue;
+    }
+  }, obj)
+}
+
+// console.log(safeGet(obj, 'user.profile.name'));      // 输出: 'Sunday'
+console.log(safeGet(obj, 'user.profile.gender', undefined)); // 输出: 'N/A'
+// console.log(safeGet(obj, 'user.address.city', 'Unknown')); // 输出: 'Unknown'
+
+const array = [15, 16, 17, 18, 19];
+
+let sum = array.reduce((acc, cur, idx) => {
+  return acc + cur
+})
+
+console.log(sum, '====')
