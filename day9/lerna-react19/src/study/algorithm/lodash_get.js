1a227e0414c38f7c24052bc8d313aca07cb459b4
leilei405 <1601178425@qq.com>
2025-03-04 14:48:18 +0800

    feat: 手动实现 正则匹配

--- day9/lerna-react19/src/study/algorithm/lodash_get.js
+++ day9/lerna-react19/src/study/algorithm/lodash_get.js
@@ -1,5 +1,5 @@
 function lodash_get (obj, path, defaultValue) {
-  const pathArray = Array.isArray(path) ? path : path.replace(/\[([^\]]+)]/, '.$1').split('.');
+  const pathArray = Array.isArray(path) ? path : path.replace(/\[([^\]]+)]/g, '.$1').split('.');
 
   return pathArray.reduce((acc, key) => {
     console.log(acc, 'acc')
@@ -18,9 +18,12 @@ const obj = {
     }
   },
   b: {
-    c: ['4', '5']
+    c: [
+      { d: 1, c: ['1', '2'] },
+    ]
   }
 }
 
 console.log(lodash_get(obj, 'a.b.c', 'dede'), '==111==')
-console.log(lodash_get(obj, 'b.c[0]', 'dede'), '==222==')
+console.log(lodash_get(obj, 'b.c[0].c[1]', 'dede'), '==222==')
+let reg = /\[([^\]]+)]/g
